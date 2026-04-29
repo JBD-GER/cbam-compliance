6ef3fe990fe375f06aff44d1f097d7134f607390
JBD-GER <info@justbedigital.de>
2026-04-29 09:06:37 +0200

    dasda

--- lib/googleAds.ts
+++ lib/googleAds.ts
@@ -1,2 +1,3 @@
 export const googleAdsId = process.env.NEXT_PUBLIC_GOOGLE_TAG_ID || "AW-18126774359";
-export const googleAdsConversionSendTo = process.env.NEXT_PUBLIC_GOOGLE_ADS_CONVERSION_SEND_TO || "";
+export const googleAdsConversionSendTo =
+  process.env.NEXT_PUBLIC_GOOGLE_ADS_CONVERSION_SEND_TO || "AW-18126774359/BHBRCOHSyqQcENfAwsND";
